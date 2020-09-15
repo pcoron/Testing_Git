f927f7029f60f10834abf26e9d90765caf5b3c6c
DESKTOP-LVH3K8J\P C <priscillacoron@gmail.com>
2020-09-15 16:13:33 -0400

    cont test

---
+++ src/main.rs
@@ -0,0 +1,7 @@
+//Fib fucking asshole. It starts with two 1's...a lot of recursion but that's not
+//good for larger numbers lo.. Idk. I am just not in the mood at the moment.
+// I'm suddenly very interested right now because the bottom is making...stuff.
+
+fn main () {
+
+}
